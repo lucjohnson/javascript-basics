a7f26af13d15a16875b654495055a6b5e5be7997
iGuest <lucj@uw.edu>
2014-10-21 09:27:12 -0700

    second day javascript

--- js/script.js
+++ js/script.js
@@ -41,3 +41,27 @@ elem.alt = 'picture of a cool cat';
 //adds this to the body of the page
 document.body.appendChild(kitty);
 
+function onPuppyClick() {
+	//alert('you clicked my puppy');
+	document.getElementById('bark').play();
+} //onPuppyClick()
+
+//selecting the puppy-pic element
+var pup = document.getElementById('puppy-pic');
+
+if (pup.addEventListener) {
+	pup.addEventListener('click', onPuppyClick)
+}
+else if (pup.attachEvent) {
+	pup.attachEvent('click', onPuppyClick)
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+
+ //add event listeners for elements
+
+ //manipulate the DOM
+ 	alert("The DOM is ready!");
+ 	onPuppyClick();
+
+});
